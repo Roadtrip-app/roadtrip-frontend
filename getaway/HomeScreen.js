5e3d58bcb3c310dd840263a8c7a4bf307e0fd4ea
Hunter DeArment <hdearment25@gmail.com>
2023-07-23 08:09:34 -0600

    feed fixes

--- getaway/HomeScreen.js
+++ getaway/HomeScreen.js
@@ -6,29 +6,21 @@ import { states } from './components/LikeScreen';
 
 
 
-export const FeedItem = ({state1, state2}) => (
+export const FeedItem = ({state1, state2, text}) => (
   <View style={styles.socialMediaContainer}>
     <TouchableOpacity style={styles.profilePic}></TouchableOpacity>
     <View style={styles.contentArea}>
-      <View style={styles.toFrom}>
-        <View style={styles.imgContain}>
-          <ImageBackground source={state1.image} style={styles.img}>
-            <Text style={styles.feedTitleText}>{state1.name}</Text>
-            <View style={styles.iconArrowContainer}>
-              <Icon name="arrow-right" size={10} color="orange" style={styles.arrowIcon} /> 
-            </View>
-          </ImageBackground>
-        </View>
-        
-        <View style={styles.imgContain}>
-          <ImageBackground source={state2.image} style={styles.img}>
-            <Text style={styles.feedTitleText}>{state2.name}</Text>
-          </ImageBackground>
-        </View>
-
+      <View style={styles.postTextArea}>
+        <Text style={styles.postText}>{text}</Text>
       </View>
-      <View style={styles.postArea}>
-        <Text style={styles.postText}>Went on a awesome trip! Saw a lot of cool stuff, and ate a ton of great food check it out!</Text>
+
+      <View style={styles.imgContainer}>
+        <ImageBackground source={state1.image} style={styles.feedImg} resizeMode="cover"></ImageBackground>
+          <Text style={styles.feedTitleText}>{state1.name}</Text>
+          <Icon name="arrow-right" size={12} color="orange" style={styles.arrowIcon} />
+        <ImageBackground source={state2.image} style={styles.feedImg} resizeMode="cover">
+          <Text style={styles.feedTitleText}>{state2.name}</Text>
+        </ImageBackground>
       </View>
     </View>
   </View>
@@ -43,7 +35,7 @@ const HomeScreen = () => (
     </View>
 
     <ScrollView style={styles.Feed}>
-      <FeedItem state1={states.colorado} state2={states.california}/>
+      <FeedItem state1={states.colorado} state2={states.california} text={"Went on an awesome trip to California"}/>
       <FeedItem state1={states.georgia} state2={states.alabama}/>
       <FeedItem state1={states.washington} state2={states.maine}/>
       <FeedItem state1={states.texas} state2={states.minnesota}/>
@@ -140,72 +132,61 @@ const styles = StyleSheet.create({
     },
 
     socialMediaContainer: {
-      backgroundColor: 'rgba(255, 255, 255, 0.8)',
+      backgroundColor: 'rgba(255, 255, 255, 0.9)',
       borderRadius: 2,
       borderWidth: 1,
       flexDirection: 'row',
-      height: '30%',
+      height: 100,
       width: '100%',
       borderColor: 'orange',
 
     },
     
     profilePic: {
-      width: 70,
-      height: 70,
+      width: 65,
+      height: 65,
       borderWidth: 1,
       borderRadius: 50,
       alignSelf: 'center',
-      left: 10,
+      left: 5,
 
     },
 
     contentArea: {
       flexDirection: 'column',
-    },
-    
-    toFrom: {
-      flexDirection: 'row',
-
-      height: 22,
-      left: 15,
-      top: 1
-
-    },
-    imgContain: {
-      
-    },
-    
-    img: {
-      width: 110,
-      height: 25,
       borderWidth: 1,
-      zIndex: 1
+      borderRadius: 3,
+      flex: 1,
+      left: 10,
+    },
+
+    postTextArea: {
+      height: 70,
+      padding: 2
+    },
+
+    postText: {
+      fontSize: 12,
+    },
+
+    imgContainer: {
+      flex: 1,
+      flexDirection: 'row',
+    },
+    feedImg: {
+      width: 117
     },
     feedTitleText: {
-      color: 'black',
-      alignSelf: 'center',
-      backgroundColor: 'rgba(255, 255, 255, 0.7)',
-      borderColor: 'black',
-      borderWidth: 1,
-      borderRadius: 5,
+      position: 'absolute',
+      color: 'white',
+      alignContent: 'center'
     },
-    postArea: {
-      borderRadius: 5,
-      left: 15,
-      width: '100%',
-      height: 75,
-      top: 35,
-      position: 'absolute'
-    },
-    postText: {
-      fontSize: 12
-    },
-    iconArrowContainer: {
-      left: 100,
-      bottom: 15,
-      zIndex: 2,
+    arrowIcon: {
+      position: 'absolute',
+      left: 100
     }
+    
+    
 
 });
 
